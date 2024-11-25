35d9c6af054f2f3d15905b6fc4328d8ebbeb46b9
reigadegr <272268642@qq.com>
2024-11-25 17:58:56 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,46 @@
-use serde::Deserialize;
-use std::fs::File;
-use std::io::{self, Read};
-
-use std::collections::HashMap;
-
-
-#[derive(Debug, Deserialize)]
-pub struct GameList {
-    #[serde(deserialize_with = "deserialize_game_list")]
-    games: HashMap<String, Vec<u32>>,
+use regex::Regex;
+use std::time::Duration;
+use std::{env, fs, process, thread};
+mod utils;
+use crate::utils::get_top_app::get_topapp_pid_and_name;
+use anyhow::Result;
+fn main() -> Result<()> {
+    // 从文件中读取TOML内容
+    let self_pid = process::id();
+    let _ = fs::write("/dev/cpuset/background/cgroup.procs", self_pid.to_string());
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("参数数量小于2，请提供至少一个参数。");
+        return Ok(());
+    }
+    let rs = read_file(args[1].clone());
+    if rs.is_err() {
+        println!("出错啦读取文件");
+    }
+    Ok(())
 }
 
-fn deserialize_game_list<'de, D>(deserializer: D) -> Result<HashMap<String, Vec<u32>>, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    use serde::de::{self, MapAccess};
-    use std::collections::HashMap;
-    
+fn read_file(file: String) -> Result<()> {
+    let config_str = fs::read_to_string(file)?;
+    let re = Regex::new(r#""(.*?)""#)?;
 
-    struct GameListVisitor;
+    // 找到所有匹配的内容
+    let matches = re.find_iter(&config_str);
 
-    impl<'de> de::Visitor<'de> for GameListVisitor {
-        type Value = HashMap<String, Vec<u32>>;
+    // 打印所有匹配的内容
+    for mat in matches {
+        println!("{}", mat.as_str());
+    }
+    Ok(())
+}
+fn run() -> Result<()> {
+    let global_package = "".to_string();
+    loop {
+        let (pid, name) = get_topapp_pid_and_name()?;
 
-        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            formatter.write_str("a map of game packages to their frame rates")
-        }
-
-        fn visit_map<V>(self, mut map: V) -> Result<HashMap<String, Vec<u32>>, V::Error>
-        where
-            V: MapAccess<'de>,
-        {
-            let mut games = HashMap::new();
-            while let Some((key, value)) = map.next_entry::<String, Vec<u32>>()? {
-                games.insert(key, value);
-            }
-            Ok(games)
+        if global_package == name {
+            thread::sleep(Duration::from_millis(1000));
+            continue;
         }
     }
-
-    deserializer.deserialize_map(GameListVisitor)
-}
-
-
-#[derive(Debug, Deserialize)]
-pub struct Setting {
-    pub game_list: GameList,
-}
-
-
-fn main() -> io::Result<()> {
-    // 打开并读取TOML文件
-    let mut file = File::open("games.toml")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    // 将字符串内容反序列化为Setting结构体
-    let setting: Setting = toml::from_str(&contents)?;
-
-    // 打印解析后的结构体
-    println!("{:#?}", setting);
-
-    Ok(())
 }
---
+++ src/utils/mod.rs
@@ -0,0 +1,1 @@
+pub mod get_top_app;
