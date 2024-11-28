98df6753cc9b89393ea40bd86b3b93ba4b9da1a6
reigadegr <272268642@qq.com>
2024-11-29 01:08:36 +0800

    nohup形式调用系统命令

--- src/run/run_cmd.rs
+++ src/run/run_cmd.rs
@@ -1,8 +1,18 @@
 //From ab/SummerSK/puff
 use std::process::Command;
 
-pub fn set_sampling_rate(rate: &str) {
-    let _ = Command::new("touchHidlTest")
-        .args(["-c", "wo", "0", "182", rate])
+pub fn set_sampling_rate(sampling_rate: &str) {
+    let _ = Command::new("nohup")
+        .args([
+            "touchHidlTest",
+            "-c",
+            "wo",
+            "0",
+            "182",
+            sampling_rate,
+            ">/dev/null",
+            "2>&1",
+            "&",
+        ])
         .output();
 }
